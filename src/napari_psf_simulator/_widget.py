bd210c82b85acd4a5623de1a8c54dda23deb849a
andreabassi78 <andreabassi@polimi.it>
2022-04-14 20:36:28 +0200

    import _fixed

--- src/napari_psf_simulator/_widget.py
+++ src/napari_psf_simulator/_widget.py
@@ -262,11 +262,11 @@ class Psf_widget(QWidget):
                      name=self.gen.write_name(basename = f'xy_{text}'),
                      colormap='twilight') 
         
-# if __name__ == '__main__':
-#     import napari
-#     viewer = napari.Viewer()
-#     widget = Psf_widget(viewer)
-#     viewer.window.add_dock_widget(widget,
-#                                   name = 'PSF Simulator @Polimi',
-#                                   add_vertical_stretch = True)
-#     napari.run()      
+if __name__ == '__main__':
+    import napari
+    viewer = napari.Viewer()
+    widget = Psf_widget(viewer)
+    viewer.window.add_dock_widget(widget,
+                                  name = 'PSF Simulator @Polimi',
+                                  add_vertical_stretch = True)
+    napari.run()      

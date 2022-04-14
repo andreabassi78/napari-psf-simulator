436283a03dd38cef06f255554ad4e7412084e2ed
andreabassi78 <andreabassi@polimi.it>
2022-04-14 17:55:06 +0200

    testing my_widget no capital

--- src/napari_psf_simulator/_tests/test_widget.py
+++ src/napari_psf_simulator/_tests/test_widget.py
@@ -2,7 +2,7 @@ from napari_psf_simulator import Psf_widget
 
 # make_napari_viewer is a pytest fixture that returns a napari viewer object
 # capsys is a pytest fixture that captures stdout and stderr output streams
-def test_PSF_widget(make_napari_viewer, capsys):
+def test_psf_widget(make_napari_viewer, capsys):
     # make viewer and add an image layer using our fixture
     viewer = make_napari_viewer()
     

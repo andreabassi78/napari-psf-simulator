a006a70e9ef10dcedee699e04ebc0136933ced25
andreabassi78 <andreabassi@polimi.it>
2023-01-31 16:09:09 +0100

    testing submodules

--- src/napari_psf_simulator/_widget.py
+++ src/napari_psf_simulator/_widget.py
@@ -4,9 +4,9 @@ Created on Sat Jan 22 00:16:58 2022
 
 @author: Andrea Bassi @Polimi
 """
-from .psf_generator import PSF_simulator
-from .gui_utils import Setting, Combo_box
-from .aberrations import Aberrations
+from psf_submodules.psf_generator import PSF_simulator
+from psf_submodules.gui_utils import Setting, Combo_box
+from psf_submodules.aberrations import Aberrations
 from qtpy.QtCore import Qt
 from qtpy.QtWidgets import QSplitter, QWidget, QPushButton
 from qtpy.QtWidgets import QLabel, QVBoxLayout, QCheckBox

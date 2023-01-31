3ac60251a847ab1df725a6cb01c2dc43fff39c7d
andreabassi78 <andreabassi@polimi.it>
2023-01-31 18:59:15 +0100

    testing try except import and testing

--- src/napari_psf_simulator/_tests/test_widget.py
+++ src/napari_psf_simulator/_tests/test_widget.py
@@ -1,4 +1,4 @@
-from .._widget import Psf_widget
+from napari_psf_simulator import Psf_widget
 
 # make_napari_viewer is a pytest fixture that returns a napari viewer object
 # capsys is a pytest fixture that captures stdout and stderr output streams
